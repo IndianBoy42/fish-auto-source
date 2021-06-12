c24995982c357112a6dbe56d26ca9a1b39c03316
Anshuman Medhi <amedhi@connect.ust.hk>
2021-06-13 03:25:34 +0800

    Update fish_auto_source.fish

--- conf.d/fish_auto_source.fish
+++ conf.d/fish_auto_source.fish
@@ -2,13 +2,15 @@
 # https://github.com/fish-shell/fish-shell/issues/5707#issuecomment-467331991
 
 function auto_source --on-event fish_prompt -d 'auto source config.fish if gets modified!'
-    set -l fish_config_time_new (date -r $FISH_CONFIG_PATH)
-    if ! set -q FISH_CONFIG_TIME
-        set -g FISH_CONFIG_TIME $fish_config_time_new
-    else
-        if test "$FISH_CONFIG_TIME" != "$fish_config_time_new"
-            set FISH_CONFIG_TIME $fish_config_time_new
-            source $FISH_CONFIG_PATH
+    if set -q FISH_CONFIG_PATH
+        set -l fish_config_time_new (date -r $FISH_CONFIG_PATH)
+        if ! set -q FISH_CONFIG_TIME
+            set -g FISH_CONFIG_TIME $fish_config_time_new
+        else
+            if test "$FISH_CONFIG_TIME" != "$fish_config_time_new"
+                set FISH_CONFIG_TIME $fish_config_time_new
+                source $FISH_CONFIG_PATH
+            end
         end
     end
 end
